733c23bd22840ecafa167db75a3472b1c4cb0271
Ben Greanya <bengreanya@gmail.com>
2022-12-14 10:13:54 -0800

    added class for theme in render utils

--- render-utils.js
+++ render-utils.js
@@ -1,4 +1,5 @@
 export function renderHaiku(haiku) {
+    const div = document.createElement('div');
     const h3 = document.createElement('h3');
     const p = document.createElement('p');
     const p2 = document.createElement('p');
@@ -9,8 +10,11 @@ export function renderHaiku(haiku) {
     p.textContent = haiku.poem[0];
     p2.textContent = haiku.poem[1];
     p3.textContent = haiku.poem[2];
+    div.classList.add(`${haiku.themes}`);
     // img.src = haiku.theme;
-    a.append(p, p2, p3);
-    //add img//
+    div.append(p, p2, p3);
+    a.append(div);
+    console.log('haiku', haiku);
+    console.log('div', div);
     return a;
 }
